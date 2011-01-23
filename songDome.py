8e3a46ec7b20c563de31d674bf7f63d5d65e5a96
Ben Fields <b.fields@gold.ac.uk>
2011-01-23 22:10:57 +0100

    removed the gross resampler that never worked

--- songDome.py
+++ songDome.py
@@ -10,17 +10,24 @@ Created by Benjamin Fields on 2011-01-23.
 Copyright (c) 2011 Goldsmith University of London. All rights reserved.
 """
 
+try:
+	import cgitb
+	cgitb.enable()
+except ImportError:
+    sys.stderr = sys.stdout
+
+print "content-type: text/html\n\n"
+
 import sys
 import cgi
 import urllib2
-
-import cgitb
-cgitb.enable()
+import random
 
 from simplejson import loads, dumps
 
 import echonest.audio as audio
 import scikits.samplerate as src
+
 
 from echonest.selection import fall_on_the
 
@@ -29,8 +36,6 @@ from my_key import EN_API_KEY #a file inline that contains an echonest key
 CACHE_DIR = "./cache/"
 
 
-# track selection methods
-LASTFM_POP = 0
 
 help_message = '''
 The help message goes here.
@@ -48,24 +53,23 @@ def zipSongs(songA, songB, outfile_name, battle_length = 8, random_order = False
 	if random_order is True then songA will still always win but the playorder will be reversed approximately half the time.
 
 	"""
-	if random_order == True:
-		raise NotImplementedError
-	onesA = songA.analysis.beats.that(fall_on_the(1))
-	onesB = songB.analysis.beats.that(fall_on_the(1))
-	
-	if songA.sampleRate != songB.sampleRate: #wooo!
-		print "changing songB sample rate of", songB.sampleRate, "to", songA.sampleRate
-		print 'songB has shape of', songB.data.shape
-		songB.data = src.resample(songB.data, float(songA.sampleRate)/songB.sampleRate, 'sinc_medium')
-		songB.sampleRate = songA.sampleRate
-		print 'songB has shape of', songB.data.shape
+	if random_order == True and random.sample([True, False], 1)[0]:
+		firstSong = songA.analysis.beats.that(fall_on_the(1))
+		secondSong = songB.analysis.beats.that(fall_on_the(1))
+		flipped = True
+	else:
+		flipped = False
+	onesA = firstSong.analysis.beats.that(fall_on_the(1))
+	onesB = secondSong.analysis.beats.that(fall_on_the(1))
 		
 	lacedUp = audio.AudioData(shape=
-		(int(1.2*len(songA.data)),2), numChannels=songA.numChannels, sampleRate=songA.sampleRate)
+		(int(1.2*len(firstSong.data)),2), numChannels=firstSong.numChannels, sampleRate=firstSong.sampleRate)
 	for bar_num in xrange(battle_length):
-		lacedUp.append(audio.getpieces(songA, [onesA[bar_num]]))
-		lacedUp.append(audio.getpieces(songB, [onesB[bar_num]]))
-	lacedUp.append(audio.getpieces(songA,songA.analysis.bars[battle_length:]))
+		lacedUp.append(audio.getpieces(firstSong, [onesA[bar_num]]))
+		lacedUp.append(audio.getpieces(secondSong, [onesB[bar_num]]))
+	if flipped:
+		lacedUp.append(audio.getpieces(firstSong, [onesA[5]]))
+	lacedUp.append(audio.getpieces(SongA,SongA.analysis.bars[battle_length:]))
 	lacedUp.encode(outfile_name)
 	return
 
@@ -85,19 +89,21 @@ def fetch7DigUrl(artist, sort_order = "song_hotttnesss-desc"):
 	#this should blow up if the track doesn't exist, should handle that in a reasonable way
 	return loads(out)['response']['songs'][0]['tracks'][0]['preview_url']
 
+def grabSongForArtist(artist, emphasis = song_hotttnesss-desc):
+
 def main(argv=None):
 	if argv==None:
-		argv = sys.argv()
-	if not len(argv) in [2,3,4]:
-		raise Usage('not enough args...')
-	artistA = argv[0]
-	artistB = argv[1]
-	if len(argv) == 3:
-		select_track_by = argv[2]
-	else:
-		select_track_by = LASTFM_POP
-
-	
+		form = cgi.FieldStorage()
+		artistA = form.getfirst("artist1")
+		artistB = form.getfirst("artist2")
+		if None in [artistA, aritistB]:
+			print dumps({'return_code':'error::give two artists.'}) 
+			return
+		select_track_by = form.getfirst("select", "song_hotttnesss-desc")
+		try:
+			battle_length = int(form.getfirst("battle_length", "8"))
+		except ValueError:
+			print
 	songA = selectArtistSong(artistA) #returns the echonest audio object for the song
 	songB = selectArtistSong(artistB)
 	
